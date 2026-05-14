481edc8c9b561924ea2493647d36bddee9ed03f5
Andrew Graves <andrewgraves92@gmail.com>
2026-05-14 11:41:30 -0400

    use C structs for register defs

--- 5_Makefiles/main.c
+++ 5_Makefiles/main.c
@@ -1,20 +1,28 @@
-#define PERIPH_BASE (0x40000000UL)
-#define AHB1PERIPH_OFFSET (0x00020000UL)
-#define AHB1PERIPH_BASE (PERIPH_BASE + AHB1PERIPH_OFFSET)
+#include <stdint.h>
 
-#define GPIOA_OFFSET (0x0000UL)
-#define GPIOA_BASE (AHB1PERIPH_BASE + GPIOA_OFFSET)
+typedef struct {
+  volatile uint32_t MODER;    // offset 0x00
+  volatile uint32_t OTYPER;   // offset 0x04
+  volatile uint32_t OSPEEDER; // offset 0x08
+  volatile uint32_t PUPDR;
+  volatile uint32_t IDR;
+  volatile uint32_t ODR;
+  volatile uint32_t BSRR;
+  volatile uint32_t LCKR;
+  volatile uint32_t AFRL;
+  volatile uint32_t AFRH;
+} GPIO_TypeDef;
 
-#define RCC_OFFSET (0x3800UL)
-#define RCC_BASE (AHB1PERIPH_BASE + RCC_OFFSET)
-#define AHB1EN_R_OFFSET (0x30UL)
-#define RCC_AHB1EN_R (*(volatile unsigned int *)(RCC_BASE + AHB1EN_R_OFFSET))
+typedef struct {
+  volatile uint32_t DUMMY[12]; // unused parts for now: 4 bytes x 12 = 48
+  volatile uint32_t AHB1ENR;   // offset 0x30
+} RCC_TypeDef;
 
-#define MODE_R_OFFSET (0x00UL)
-#define GPIOA_MODE_R (*(volatile unsigned int *)(GPIOA_BASE + MODE_R_OFFSET))
+#define RCC_BASE 0x40023800
+#define GPIOA_BASE 0x40020000
 
-#define OD_R_OFFSET (0x14UL)
-#define GPIOA_OD_R (*(volatile unsigned int *)(GPIOA_BASE + OD_R_OFFSET))
+#define RCC ((RCC_TypeDef *)RCC_BASE)
+#define GPIOA ((GPIO_TypeDef *)GPIOA_BASE)
 
 #define GPIOAEN (1U << 0)
 #define PIN5 (1U << 5)
@@ -23,18 +31,18 @@
 
 int main(void) {
   // clock enable for the bus where PA5 lives
-  RCC_AHB1EN_R |= GPIOAEN;
+  RCC->AHB1ENR |= GPIOAEN;
 
   // set pins 10 and 11 to 1 and 0 respectively
   // this indicates general output mode for PA5
-  GPIOA_MODE_R |= (1U << 10);
-  GPIOA_MODE_R &= ~(1U << 11);
+  GPIOA->MODER |= (1U << 10);
+  GPIOA->MODER &= ~(1U << 11);
 
   while (1) {
     // set PA5 HIGH
-    GPIOA_OD_R ^= LED_PIN;
+    GPIOA->ODR ^= LED_PIN;
 
-    for (int i = 0; i < 100000; i++) {
+    for (int i = 0; i < 5000000; i++) {
       // do nothing
     }
   }
